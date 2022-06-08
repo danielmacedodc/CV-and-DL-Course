8363eaa588a81eae300e6a66952d4c8b3b6953a9
Macecode <danielmacedo.dc@gmail.com>
2022-06-08 10:28:58 -0300

    update in model 4 and new function to improve data loading

--- NLP Project/help_me.py
+++ NLP Project/help_me.py
@@ -7,6 +7,9 @@
 import sklearn
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_fscore_support
 
+import tensorflow
+
+# function to calculate metrics
 def calculate_results(y_true, y_pred):
     """
     Calculates model accuracy, precision, recall and f1-score of binary classification model.
@@ -27,3 +30,27 @@ def calculate_results(y_true, y_pred):
                     'f1': model_f1}
     
     return model_results
+
+# function to improve data loading into the model
+def data_loading_pipeline(X, y, batch_size=32):
+    
+    """
+    Function that creates a dataset in order to improve data loading into the model.
+    Args:
+        X: Original data
+        y: labels
+        batch_size: default (32)
+        
+    returns:
+        prefetched dataset
+    """
+    
+    # creating dataset
+    data = tf.data.Dataset.from_tensor_slices(X)
+    labels = tf.data.Dataset.from_tensor_slices(y)
+    dataset = tf.data.Dataset.zip((data, labels))
+    
+    # prefetch and batch
+    dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    
+    return dataset
